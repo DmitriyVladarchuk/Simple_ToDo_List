42aa1d59f848198d9762111f833f98df9ab3b685
unknown <dimadf678@gmail.com>
2024-03-09 16:33:04 +0300

    add bottom menu and navigation

--- app/src/main/java/com/example/simpletodolist/MainActivity.kt
+++ app/src/main/java/com/example/simpletodolist/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.simpletodolist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavMenu = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavMenu.setupWithNavController(navController)
     }
 }
